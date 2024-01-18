ece6e633500535ec17afdf5eab1df87d6cb98d79
Viktor Skarlatov <viktor.skarlatov@gmail.com>
2024-01-18 15:16:26 +0200

    Changed to commonjs. Jest sucks.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import dts from 'rollup-plugin-dts';
+import commonjs from '@rollup/plugin-commonjs';
 
 const baseConfig = createBasicConfig();
 
@@ -10,12 +11,13 @@ export default merge(baseConfig, [
     input: './js/src/index.js',
     output: {
       dir: 'dist',
-      format: 'esm',
+      format: 'cjs',
     },
     external: [
       'react'
     ],
     plugins: [
+      commonjs(),
       nodeResolve(),
     ],
   },
