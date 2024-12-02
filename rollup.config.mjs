da2e271ff718e8142cfa2e52baca4896c16a1276
Viktor Skarlatov <viktor.skarlatov@gmail.com>
2024-12-02 19:27:16 +0200

    Moving to es modules.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import dts from 'rollup-plugin-dts';
-import commonjs from '@rollup/plugin-commonjs';
 
 const baseConfig = createBasicConfig();
 
@@ -11,13 +10,12 @@ export default merge(baseConfig, [
     input: './js/src/index.js',
     output: {
       dir: 'dist',
-      format: 'cjs',
+      format: 'es'
     },
     external: [
       'react'
     ],
     plugins: [
-      commonjs(),
       nodeResolve(),
     ],
   },
